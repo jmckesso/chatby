d7ba0d7818f434a4aedc217a8c4a2331fe42a632
jmckesso <jmckesso@purdue.edu>
2017-02-21 23:59:21 -0500

    should inlude all new files.  code in view controller to receive JSON

--- chatby/ViewController.swift
+++ chatby/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    
+    var list_of_users = "http://chatby.vohras.tk/api/users"
 
+    @IBOutlet weak var username_field: UITextField!
+    @IBOutlet weak var password_field: UITextField!
+    
+    var names = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        callAlamo( url: list_of_users )
+        
     }
-
+    
+   func callAlamo( url: String ) {
+        Alamofire.request( url ).responseJSON(completionHandler: {
+            response in
+            print(response.result)
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        })
+    }
+    
+    @IBAction func login_button(_ sender: UIButton) {
+        
+        Alamofire.request(list_of_users).responseJSON(completionHandler: {
+            response in
+            
+            do {
+                let readable_json = try JSONSerialization.jsonObject(with: response.data!) as? [[String: Any]]
+                print(readable_json)
+            }
+            catch {
+                print(error)
+            }
+            
+            //let users = readable_json["username"] as! String
+            ///       print(users)
+            
+        })
+        
+        
+        if (username_field.text == "Jacob") {
+            print("username set correctly")
+        }
+        else {
+            print("username not set correctly")
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
