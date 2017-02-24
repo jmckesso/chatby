531db5d072728326ef0513fce7257495bb73a099
Tanner Strom <tstrom@purdue.edu>
2017-02-23 21:43:00 -0500

    changed a key on a keyboard and made some fields bigger

--- chatby/LoginViewController.swift
+++ chatby/LoginViewController.swift
@@ -141,6 +141,7 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         username.layer.masksToBounds = true;
         username.placeholder = "Username"
         username.textAlignment = NSTextAlignment.center;
+        username.returnKeyType = UIReturnKeyType.done;
         username.keyboardType = UIKeyboardType.emailAddress;
         username.alpha = 1.0;
         username.autocorrectionType = UITextAutocorrectionType.no;
@@ -165,6 +166,7 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         password.layer.masksToBounds = true;
         password.placeholder = "Password"
         password.textAlignment = NSTextAlignment.center;
+        username.returnKeyType = UIReturnKeyType.done;
         password.isEnabled = true;
         password.isSecureTextEntry = true;
         password.alpha = 1.0;
