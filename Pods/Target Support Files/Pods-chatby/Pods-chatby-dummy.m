5bced9635b7e948191c912ba0e71337291dd1952
Tanner Strom <tstrom@purdue.edu>
2017-03-29 22:28:30 -0400

    xcode didnt push everything...?

---
+++ Pods/Target Support Files/Pods-chatby/Pods-chatby-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatby : NSObject
+@end
+@implementation PodsDummy_Pods_chatby
+@end
---
+++ Pods/Target Support Files/Pods-chatbyTests/Pods-chatbyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatbyTests : NSObject
+@end
+@implementation PodsDummy_Pods_chatbyTests
+@end
